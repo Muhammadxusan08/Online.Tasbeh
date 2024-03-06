01d3947e09fcabf8ee72c42d9f76d3c35adbb653
Muhammadxusan08 <fotbol100k@gmail.com>
2024-03-06 18:11:03 +0300

    tugatilish

---
+++ index.js
@@ -0,0 +1,20 @@
+let btn1 = document.querySelector('.button-1')
+let btn2 = document.querySelector('.button-2')
+let btn3 = document.querySelector('.button-3')
+let text = document.querySelector('.card-text')
+
+
+
+btn2.addEventListener('click', function () {
+    text.textContent = text.textContent *1+1
+})
+
+btn3.addEventListener('click' , function(){
+    text.textContent = '0'
+    
+})
+
+btn1.addEventListener('click' , function(){
+    text.textContent = '0'
+
+})
